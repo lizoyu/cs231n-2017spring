b9b576c9c42d4fcba0121d931e83958148d94356
Chungyuen Li <lizoyu1@gmail.com>
2017-08-14 10:29:41 -0500

    add assignment1 and assignment2

---
+++ assignment1/cs231n/classifiers/softmax.py
@@ -0,0 +1,107 @@
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+
+def softmax_loss_naive(W, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  # compute the loss
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    exp_sum = 0.0
+    max_score = np.max(scores)
+    for score in scores:
+      exp_sum += np.exp(score-max_score)
+    loss -= np.log(np.exp(scores[y[i]]-max_score)/exp_sum)
+    for j in xrange(num_classes):
+      t = 0
+      if j == y[i]:
+        t = 1
+      dW[:,j] += X[i]*(np.exp(scores[j]-max_score)/exp_sum - t)
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+  dW += 2 * reg * W
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+  return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  scores = X.dot(W)
+  scores = np.exp(scores-np.max(scores,1).reshape((num_train,1)))
+
+  # compute the loss
+  exp_sum = np.sum(scores,1)
+  loss = -np.sum(np.log(scores[range(num_train),y]/exp_sum))
+
+  # compute the gradient
+  target = np.zeros_like(scores)
+  target[range(num_train),y] = 1
+  dW = X.T.dot(scores/exp_sum.reshape((num_train,1)) - target)
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+  dW += 2 * reg * W
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
